55468b32f619a9c57e497909f8fa6f829d545b70
Kıvanç Onat Türker <kivo0326@gmail.com>
2025-01-29 00:22:32 +0300

    Java version change from 21 to 17

---
+++ src/main/java/com/school/database/school/controller/RootController.java
@@ -0,0 +1,15 @@
+package com.school.database.school.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+
+    @GetMapping
+    public String home() {
+        return "Welcome to the School API!";
+    }
+}
